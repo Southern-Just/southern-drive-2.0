e816800efe779a9e1f9202a0a757ff601bd3af9b
Southern-Just <karuguezekiel@gmail.com>
2025-01-23 19:21:48 +0300

    Afresh commit,00

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen bg-brand">
+      <h1 className="h1">Southern Drive :: Winter is coming</h1>
+    </div>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,29 @@
+import type { NextConfig } from "next";
+import { hostname } from "os";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+    ],
+  },
+};
+
+
+export default nextConfig;
