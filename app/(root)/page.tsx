47ed95399d4fa9fb337f820f79612198ab469c8c
Southern-Just <karuguezekiel@gmail.com>
2025-01-24 19:53:41 +0300

    Dynamic Routing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex-center h-screen bg-brand">
+    <div className="flex-center h-screen">
       <h1 className="h1">Southern Drive :: Winter is coming</h1>
     </div>
   );
